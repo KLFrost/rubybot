8fcdc0a5db7e6d665a36b6c7bbb23423b80a00d1
Keith Frost <keith@dwnldmedia.com>
2016-03-13 12:08:02 -0400

    help n stuff

--- rubybot.rb
+++ rubybot.rb
@@ -1,25 +1,64 @@
 require 'slack-ruby-bot'
 require 'redis'
 
+module SlackRubyBot
+  module Commands
+    class Help < Base
+      HELP = <<EOM
+```
+ruby sadd set_number_9 foo bar baz        add elements to a set
+ruby srem set_foo_bar  baz qux zip        remove elements from a set
+ruby sample set_zippy  10                 sample elements without replacement
+ruby sample set_zippy -10                 sample elements with replacement
+ruby hi                                   say hi, get a gif
+ruby ping                                 says pong
+...                                       ?
+```
+EOM
+      def self.call(client, data, _match)
+        client.say(channel: data.channel, text: HELP)
+      end
+    end
+
+    class Unknown < Base
+      def self.call(client, data, _match)
+        msg = $redis.srandmember('rb-set:unknown')
+        return unless msg
+        msg.gsub!('~',' ')
+        client.say(channel: data.channel, text: "<@#{data.user}> #{msg}")
+      end
+    end
+  end
+end
+
 $redis = Redis.new(url: 'redis://127.0.0.1:6379')
 class RubyBot < SlackRubyBot::Bot
   command 'ping' do |client, data, match|
-    client.say(text: 'pong', channel: data.channel)
+    pong = "<@#{data.user}> #{Time.now.utc} :pingpong: pong"
+    client.say(text: pong, channel: data.channel)
   end
+  SADD = %r{^ruby +sadd +(?<set>\w+) +(?<members>([[:graph:]]+ *)+)$}
+  SREM = %r{^ruby +srem +(?<set>\w+) +(?<members>([[:graph:]]+ *)+)$}
   
-  match(/^ruby +sadd +(?<set>\w+) +(?<member>\w+)$/i) do |client, data, match|
-    n = $redis.sadd("rb-set:#{match[:set]}", match[:member])
-    client.say(text: n.to_s, channel: data.channel)
+  match(SADD) do |client, data, match|
+    members = match[:members].split(/\s+/)
+    n = $redis.sadd("rb-set:#{match[:set]}", members)
+    if n && (1 == members.size || n > 0)
+      client.say(text: n.to_s, channel: data.channel)
+    end
   end
 
-  match(/^ruby +srem +(?<set>\w+) +(?<member>\w+)$/i) do |client, data, match|
-    n = $redis.srem("rb-set:#{match[:set]}", match[:member])
-    client.say(text: n.to_s, channel: data.channel)
+  match(SREM) do |client, data, match|
+    members = match[:members].split(/\s+/)
+    n = $redis.srem("rb-set:#{match[:set]}", members)
+    if n && (1 == members.size || n > 0)
+      client.say(text: n.to_s, channel: data.channel)
+    end
   end
 
-  match(/^ruby scard (?<set>\w+)$/i) do |client, data, match|
+  match(/^ruby +scard +(?<set>\w+) *$/i) do |client, data, match|
     n = $redis.scard("rb-set:#{match[:set]}")
-    client.say(text: n.to_s, channel: data.channel)
+    client.say(text: n.to_s, channel: data.channel) if n > 0
   end
 
   match(/^ruby sample (?<set>\w+) *(?<count>-?\d+)?$/i) do |client, data, match|
@@ -34,7 +73,7 @@ class RubyBot < SlackRubyBot::Bot
         []
       end
     unless members.empty?
-      client.say(text: members.join(', '), channel: data.channel)
+      client.say(text: members.join(' '), channel: data.channel)
     end
   end
 end
